66038ec417d79f87c35b977f10217fddc829aa1f
cihancacan <130900341+cihancacan@users.noreply.github.com>
2026-07-31 21:51:50 +0300

    Brancher les nouvelles pages SEO FR et EN

--- src/App.tsx
+++ src/App.tsx
@@ -17,14 +17,11 @@ import PrixGreffeCheveuxTurquie from './pages/seo/PrixGreffeCheveuxTurquie';
 import BeforeAfterPage from './pages/BeforeAfterPage';
 import HairTransplantTurkey from './pages/seo/HairTransplantTurkey';
 import TurkeyHairTransplantCost from './pages/seo/TurkeyHairTransplantCost';
+import SeoLandingPage from './pages/seo/SeoLandingPage';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
+  useEffect(() => window.scrollTo(0, 0), [pathname]);
   return null;
 }
 
@@ -47,35 +44,56 @@ function AppContent() {
           <Route path="/greffe-de-cheveux-turquie" element={<GreffeCheveuxTurquie />} />
           <Route path="/implant-capillaire-turquie" element={<GreffeCheveuxTurquie />} />
           <Route path="/implant-cheveux-turquie" element={<GreffeCheveuxTurquie />} />
-          <Route path="/greffe-de-cheveux-turquie-avis" element={<GreffeCheveuxTurquie />} />
-          <Route path="/meilleure-clinique-greffe-cheveux-turquie" element={<GreffeCheveuxTurquie />} />
-          <Route path="/meilleure-clinique-implant-cheveux-turquie" element={<GreffeCheveuxTurquie />} />
-          <Route path="/meilleure-clinique-implant-capillaire-turquie" element={<GreffeCheveuxTurquie />} />
+          <Route path="/meilleure-clinique-greffe-cheveux-turquie" element={<SeoLandingPage lang="fr" pageKey="reviews" />} />
+          <Route path="/meilleure-clinique-implant-cheveux-turquie" element={<SeoLandingPage lang="fr" pageKey="reviews" />} />
+          <Route path="/meilleure-clinique-implant-capillaire-turquie" element={<SeoLandingPage lang="fr" pageKey="reviews" />} />
 
           <Route path="/greffe-de-cheveux-fue-turquie" element={<GreffeCheveuxFUETurquie />} />
-          <Route path="/greffe-de-barbe-turquie" element={<GreffeCheveuxFUETurquie />} />
-
           <Route path="/greffe-de-cheveux-dhi-turquie" element={<GreffeCheveuxDHITurquie />} />
-
           <Route path="/prix-greffe-de-cheveux-turquie" element={<PrixGreffeCheveuxTurquie />} />
           <Route path="/greffe-cheveux-prix-turquie" element={<PrixGreffeCheveuxTurquie />} />
           <Route path="/prix-implant-capillaire-turquie" element={<PrixGreffeCheveuxTurquie />} />
           <Route path="/implant-cheveux-turquie-prix" element={<PrixGreffeCheveuxTurquie />} />
-          <Route path="/greffe-de-cheveux-turquie-prix-tout-compris" element={<PrixGreffeCheveuxTurquie />} />
+          <Route path="/greffe-de-cheveux-turquie-prix-tout-compris" element={<SeoLandingPage lang="fr" pageKey="allInclusive" />} />
+
+          <Route path="/greffe-cheveux-istanbul" element={<SeoLandingPage lang="fr" pageKey="istanbul" />} />
+          <Route path="/greffe-cheveux-turquie-tout-compris" element={<SeoLandingPage lang="fr" pageKey="allInclusive" />} />
+          <Route path="/greffe-cheveux-turquie-avis" element={<SeoLandingPage lang="fr" pageKey="reviews" />} />
+          <Route path="/greffe-de-cheveux-turquie-avis" element={<SeoLandingPage lang="fr" pageKey="reviews" />} />
+          <Route path="/greffe-cheveux-turquie-avant-apres" element={<SeoLandingPage lang="fr" pageKey="beforeAfter" />} />
+          <Route path="/greffe-cheveux-femme-turquie" element={<SeoLandingPage lang="fr" pageKey="women" />} />
+          <Route path="/greffe-cheveux-afro-turquie" element={<SeoLandingPage lang="fr" pageKey="afro" />} />
+          <Route path="/greffe-cheveux-crepus-turquie" element={<SeoLandingPage lang="fr" pageKey="afro" />} />
+          <Route path="/greffe-barbe-turquie" element={<SeoLandingPage lang="fr" pageKey="beard" />} />
+          <Route path="/greffe-de-barbe-turquie" element={<SeoLandingPage lang="fr" pageKey="beard" />} />
+          <Route path="/nombre-greffons-greffe-cheveux" element={<SeoLandingPage lang="fr" pageKey="grafts" />} />
+          <Route path="/greffe-cheveux-turquie-risques" element={<SeoLandingPage lang="fr" pageKey="risks" />} />
+          <Route path="/greffe-cheveux-turquie-danger" element={<SeoLandingPage lang="fr" pageKey="risks" />} />
+          <Route path="/apres-greffe-cheveux-mois-par-mois" element={<SeoLandingPage lang="fr" pageKey="recovery" />} />
 
           <Route path="/hair-transplant-turkey" element={<HairTransplantTurkey />} />
           <Route path="/turkey-hair-transplant" element={<HairTransplantTurkey />} />
           <Route path="/hair-transplant-in-turkey" element={<HairTransplantTurkey />} />
-          <Route path="/best-hair-transplant-turkey" element={<HairTransplantTurkey />} />
-          <Route path="/hair-transplant-turkey-reviews" element={<HairTransplantTurkey />} />
+          <Route path="/best-hair-transplant-turkey" element={<SeoLandingPage lang="en" pageKey="reviews" />} />
           <Route path="/fue-hair-transplant-turkey" element={<HairTransplantTurkey />} />
           <Route path="/dhi-hair-transplant-turkey" element={<HairTransplantTurkey />} />
-
           <Route path="/turkey-hair-transplant-cost" element={<TurkeyHairTransplantCost />} />
           <Route path="/hair-transplant-turkey-cost" element={<TurkeyHairTransplantCost />} />
           <Route path="/hair-transplant-turkey-price" element={<TurkeyHairTransplantCost />} />
           <Route path="/turkey-hair-transplant-prices" element={<TurkeyHairTransplantCost />} />
           <Route path="/how-much-hair-transplant-turkey" element={<TurkeyHairTransplantCost />} />
+
+          <Route path="/en/hair-transplant-istanbul" element={<SeoLandingPage lang="en" pageKey="istanbul" />} />
+          <Route path="/en/all-inclusive-hair-transplant-turkey" element={<SeoLandingPage lang="en" pageKey="allInclusive" />} />
+          <Route path="/en/hair-transplant-turkey-reviews" element={<SeoLandingPage lang="en" pageKey="reviews" />} />
+          <Route path="/hair-transplant-turkey-reviews" element={<SeoLandingPage lang="en" pageKey="reviews" />} />
+          <Route path="/en/hair-transplant-turkey-before-after" element={<SeoLandingPage lang="en" pageKey="beforeAfter" />} />
+          <Route path="/en/female-hair-transplant-turkey" element={<SeoLandingPage lang="en" pageKey="women" />} />
+          <Route path="/en/afro-hair-transplant-turkey" element={<SeoLandingPage lang="en" pageKey="afro" />} />
+          <Route path="/en/beard-transplant-turkey" element={<SeoLandingPage lang="en" pageKey="beard" />} />
+          <Route path="/en/hair-transplant-graft-count" element={<SeoLandingPage lang="en" pageKey="grafts" />} />
+          <Route path="/en/hair-transplant-turkey-risks" element={<SeoLandingPage lang="en" pageKey="risks" />} />
+          <Route path="/en/hair-transplant-recovery-timeline" element={<SeoLandingPage lang="en" pageKey="recovery" />} />
         </Routes>
       </main>
       <Footer />
@@ -84,11 +102,7 @@ function AppContent() {
 }
 
 function App() {
-  return (
-    <Router>
-      <AppContent />
-    </Router>
-  );
+  return <Router><AppContent /></Router>;
 }
 
 export default App;
